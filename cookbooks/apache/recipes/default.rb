ac3dd9b1b55ee69d0dfacadf218de48038e4dd06
mario <m@a.com>
2017-06-26 14:27:56 -0400

    apache fonctionnel

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Install apache
+package 'httpd' do
+  action :install
+end
+
+# Apache start the service.
+service "httpd" do
+  action [:restart]
+end
