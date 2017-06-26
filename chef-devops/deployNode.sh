441b2039ff824b8ad02cae675358b56d6d759f2c
pH <pierre-hugues.dorais@cgi.com>
2017-06-26 15:23:08 -0400

    delete vm si existe

--- chef-devops/deployNode.sh
+++ chef-devops/deployNode.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+#Auteurs : Pierre-Hugues Dorais, Samuel Ross, Marc Gauthier
+#Date : 26 juin 2017
 
+#Validation premier parametre.
 if [ $# -ne 1 ]; then
 	echo "Usage : `basename $0` nom_machine"
 exit 1
 fi
 
+#Variables.
 nom_machine=$1
 nom_template=vstemplate-centos7
 folder_templates=Templates
@@ -16,7 +20,15 @@ datastore=SAN_LUN3
 resource_pool=CLUSTER_INTDEV_QA_UAT_PREPROD
 destination_folder=Chef
 
+#Verification VM. Si elle existe, la supprime dans vSphere et dans Chef.
+if `knife vsphere vm list -f Chef | grep  "VM Name: $nom_machine" >/dev/null 2>&1`; then
+	knife vsphere vm delete $nom_machine -f $destination_folder
+	knife node delete $nom_machine -y
+	knife client delete $nom_machine -y
+fi
 
+
+#Creation de VM dans vSphere et "bootstrapper" dans Chef.
 knife vsphere vm clone $nom_machine --template $nom_template -f $folder_templates \
  --cips DHCP --ssh-user $ssh_user --ssh-password $ssh_password --cvlan $vlan \
  --resource-pool $resource_pool --datastore $datastore --dest-folder $destination_folder \
