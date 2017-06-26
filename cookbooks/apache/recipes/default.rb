747b1ea34f08e88ada10c68e10514ae6439566ec
mario <m@a.com>
2017-06-26 14:59:19 -0400

    apache fonctionnel v0.1

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,11 +5,15 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 # Install apache
-package 'httpd' do
+package ['httpd','telnet'] do
   action :install
 end
 
 # Apache start the service.
 service "httpd" do
   action [:restart]
+end
+
+service "firewalld" do
+  action [:disable,:stop]
 end
