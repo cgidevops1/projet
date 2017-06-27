2d916bac17ecb9209c81f3e228023418f68f272d
mario <m@a.com>
2017-06-27 14:57:18 -0400

    JP: Adding test to mdjk compilation

--- chef-devops/cookbooks/modjk/test/smoke/default/default_test.rb
+++ chef-devops/cookbooks/modjk/test/smoke/default/default_test.rb
@@ -5,14 +5,60 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+control "modjk" do
+
+  impact 0.6
+  title "Test to compiling modJK"
+  desc '
+   Will check if the compilating share object is correctly created.
+  '
+  tag "apache", "tomcat", "modjk"
+
+  describe package('gcc') do
+    it { should be_installed }
+  end
+
+  describe package('gcc-c++') do
+    it { should be_installed }
+  end
+
+  describe package('make') do
+    it { should be_installed }
+  end
+
+  describe package('libtool') do
+    it { should be_installed }
+  end
+
+  describe file('/usr/bin/apxs') do
+    it { should be_executable }
+  end
+
+  describe bash('libtool') do
     it { should exist }
   end
-end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+  describe bash('find / -name tomcat-connector-1.2.42-src.tar.gz') do
+    its('stdout') { should match "*tomcat-connector-1.2.42-src.tar.gz" }
+  end
+
+  describe file('/etc/httpd/modules/mod_jk.so') do
+    it { should exist }
+  end
+
+  # describe file('/etc/httpd/conf.d/jk.conf') do
+  #   it { should exist }
+  # end
+  #
+  # describe file('/etc/httpd/conf.d/workers.properties') do
+  #   it { should exist }
+  # end
+  #
+  # describe file('/var/log/httpd/mod_jk.shm') do
+  #   it { should exist }
+  # end
+  #
+  # describe file('/var/log/httpd/mod_jk.log') do
+  #   it { should exist }
+  # end
 end
