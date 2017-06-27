bfe0ea2a990357bd302cd3aa9c3d3c42d3135b16
Marc-André Rondeau <Marc-Andre.rondeau@cgi.com>
2017-06-27 14:31:38 -0400

    creation de role

---
+++ chef-devops/createrole.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#création de variable
+
+for roles in WEB DB APP ; do
+
+
+
+#verifié si le role est present et créer le role (web bd,tomcat... etc)
+knife role list | grep "^$roles$" > /dev/null 2>&1
+
+  if [ $? -ne 0 ]; then
+    knife role create $roles  -d --defaults
+  fi
+  for nodes in `knife node list | grep -i "^devops-$roles-"`; do
+      knife node run_list add $nodes "role[$roles]"
+  done
+done
+
+#rechercher et associer les roles au nodes
