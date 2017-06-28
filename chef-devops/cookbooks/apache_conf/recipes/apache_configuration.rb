759bebcc9436e7f24229f55cd2aa01d5206cbb95
Marc-André Rondeau <Marc-Andre.rondeau@cgi.com>
2017-06-28 08:47:28 -0400

    Premier commit

---
+++ chef-devops/cookbooks/apache_conf/recipes/apache_configuration.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: apache_conf
+# Recipe:: apache_configuration
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
---
+++ chef-devops/cookbooks/apache_conf/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: apache_conf
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
