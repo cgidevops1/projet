61732e8e8e38a2fa0827fecc5ffc10ca626c4d93
Jimmy Provencher <jprovencher@cgi.cxm>
2017-06-28 09:55:55 -0400

    Test of the settings of apache to be able to communicate with tomcat

--- chef-devops/cookbooks/apache_conf/test/smoke/default/apache_configuration_test.rb
+++ chef-devops/cookbooks/apache_conf/test/smoke/default/apache_configuration_test.rb
@@ -5,14 +5,83 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
+control "apache" do
+  impact 0.6
+  title 'Server: Test the apache configuration with Tomcat'
+  desc '
+   Test if port, user and files are in place to be able to
+   communicate with tomcat server.
+  '
+  tag 'apache', 'apache-server', "server"
+  unless os.windows?
+    # This is an example test, replace with your own test.
+    describe user('root'), :skip do
+      it { should exist }
+    end
+  end
+
+  # This is an example test, replace it with your own test.
+  describe port(80), :skip do
+    it { should_not be_listening }
+  end
+
+  #Le user apache doit exister
+  describe user('apache') do
     it { should exist }
+  end
+
+  #Verifier que httpd est enables
+  describe service('httpd.service') do
+    it { be_running }
+  end
+
+  #Verifier que httpd est enables
+  describe service('firewalld') do
+    it { should be_running }
+  end
+
+
+  #Le port 80 doit etre ouvert
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  #Verifier que le folder apache par default est present
+  describe directory("/var/www") do
+  	it { should exist }
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+control 'Apache_configuration' do
+  impact 0.6
+  title 'Server: Test the apache configuration with Tomcat'
+  desc '
+   Test if port, user and files are in place to be able to
+   communicate with tomcat server.
+  '
+  tag 'apache_conf', 'apache configuration', "server"
+
+  describe file('/etc/httpd/conf.d/jk.conf') do
+    it { should exist }
+  end
+
+  describe file('/etc/httpd/conf.d/workers.properties') do
+    it { should exist }
+  end
+
+  describe file('/var/log/httpd/mod_jk.shm') do
+    it { should exist }
+  end
+
+  describe file('/var/log/httpd/mod_jk.log') do
+    it { should exist }
+  end
+
+  describe file('/etc/httpd/modules/mod_jk.so') do
+    it { should exist }
+  end
+
+  describe service('firewalld') do
+    it { be_running }
+  end
 end
